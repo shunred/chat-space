419a00be1cbb43b47ef7523de39afb70e0b8981a
Shunya akaboshi <shunyaakaboshi@ShunyanoMacBook-puro.local>
2018-09-16 16:51:48 +0900

    メッセージビューの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -18,7 +18,7 @@ end
 
 private
 def message_params
-  params.requre(:message).permit(:body, :image).merge(user_id: current_user.id)
+  params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
 end
 
 def set_group
